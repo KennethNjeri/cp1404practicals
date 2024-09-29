e7bee1cce88648010dee45eeaf228d822f08a20b
KennethNjeri <kennethkariuki.njeri@my.jcu.edu.au>
2024-09-29 16:07:09 +1000

    Refactor scores program to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,7 +1,19 @@
-minimum_length = 6
 
-password = input("Enter a password: ")
-while len(password) < minimum_length:
-    print("Invalid password")
+def main():
+    minimum_length = 6
+    get_password(minimum_length)
+
+
+def get_password(minimum_length):
     password = input("Enter a password: ")
-print("*" * len(password))
+    while len(password) < minimum_length:
+        print("Invalid password")
+        password = input("Enter a password: ")
+    print_asterisks(password)
+
+
+def print_asterisks(password):
+    print("*" * len(password))
+
+
+main()
