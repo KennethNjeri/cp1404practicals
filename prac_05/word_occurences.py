6083c0876e5b9162da98d27cf8186256921a16d2
KennethNjeri <kennethkariuki.njeri@my.jcu.edu.au>
2024-10-20 16:13:06 +1000

    Add word frequency counter and aligned output

---
+++ prac_05/word_occurences.py
@@ -0,0 +1,22 @@
+"""
+Word Occurrences
+Estimate: 30 minutes
+Actual:
+"""
+
+WORD_TO_COUNT = {}
+text = input("Text: ")
+words = text.split()
+
+for word in words:
+    if word in WORD_TO_COUNT:
+        WORD_TO_COUNT[word] += 1
+    else:
+        WORD_TO_COUNT[word] = 1
+
+unique_words = sorted(WORD_TO_COUNT.keys())
+longest_word_length = max(len(word) for word in unique_words)
+
+for word in unique_words:
+    print(f"{word:<{longest_word_length}} : {WORD_TO_COUNT[word]}")
+
