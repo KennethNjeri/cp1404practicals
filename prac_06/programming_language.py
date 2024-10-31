3847981f2fa9174055ea3e043bd2b211f91f1629
KennethNjeri <kennethkariuki.njeri@my.jcu.edu.au>
2024-10-31 14:47:17 +1000

    Add __str__ method to ProgrammingLanguage class

---
+++ prac_06/programming_language.py
@@ -0,0 +1,18 @@
+"""
+Estimate:25 minutes
+Actual:38 minutes
+"""
+
+class ProgrammingLanguage:
+    def __init__(self,name="", typing="", reflection="", year=0):
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+    def is_dynamic(self):
+        return self.typing == "dynamic"
+
+    def __str__(self):
+        return f"{self.name},{self.typing} typing, reflection= {self.reflection},First appeared in {self.year}."
+
